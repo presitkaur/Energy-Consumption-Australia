af1cfd7227da10340e241e2bfa8989444b8cf039
Aroosa-Github <aroosa@outlook.com>
2021-04-24 13:59:42 +0800

    added files

---
+++ data.js
@@ -0,0 +1,937 @@
+var data =[{
+    year :2000-01,
+    state:nsw,
+    population:6851887,
+    GDP:1450.5,
+    energyconsumption:411286
+},
+{
+   year: 2001-02,
+   state: nsw,
+   population: 6905434,
+   GDP: 1445.7,
+   energyconsumed: 419311
+},
+{
+  year: 2002-03,
+  state: nsw,
+  population:6948072,
+  GDP: 1510.5,
+  energyconsumed:428112
+},
+
+{
+    year: 2003-04,
+    state: nsw,
+    population:6979675,
+    GDP:1574.8,
+    energyconsumed: 442714
+},
+
+{
+    year : 2004-05,
+    state: nsw,
+    population: 7024605,
+    GDP :1544.9,
+    energyconsumed:450746
+},
+
+{
+    year:2005-06, 
+    state:nsw,
+    population: 7077860,
+    GDP: 1546,
+    energyconsumed:457016
+},
+
+{
+    year :2006-07,
+    state: nsw,
+    population :7176800,
+    GDP: 1568.9,
+    energyconsumed:465480
+},
+
+{
+    year :2007-08,
+    state :nsw,
+    population :7291829,
+    GDP :1610.7,
+    energyconsumed:477101
+},
+
+{
+    year: 2008-09,
+    state :nsw,
+    population :7408540,
+    GDP:1592.2,
+    energyconsumed:482539
+},
+{
+    year :2009-10,
+    state :nsw,
+    population :7506058,
+    GDP :1643.2,
+    energyconsumed: 492315
+},
+{
+    year :2010-11,
+    state :nsw,
+    population :7586514,
+    GDP :1665.7,
+    energyconsumed :504340
+},
+{
+    year : 2011-12,
+    state :nsw,
+    population :7680783,
+    GDP :1619.6,
+    energyconsumed:516038
+},
+{
+    year : 2012-13,
+    state :nsw,
+    population :7787289,
+    GDP :1566.2,
+    energyconsumed:525816
+},
+{
+    year :2013-14,
+    state :nsw,
+    population :7897152,
+    GDP: 1514.2,
+    energyconsumed:535464
+},
+{
+    year :2014-15,
+    state: nsw,
+    population :8011981,
+    GDP :1467.6,
+    energyconsumed:549534
+},
+{
+    year :2015-16,
+    state :nsw,
+    population:8135962,
+    GDP:1514.7,
+    energyconsumed:570349
+},
+{
+    year:2016-17,
+    state :nsw,
+    population:8279961,
+    GDP:1525.3,
+    energyconsumed:588454
+},
+{
+    year :2017-18,
+    state :nsw,
+    population:8400547,
+    GDP:1545.1,
+    energyconsumed:603143
+},
+{
+    year :2018-19,
+    state:nsw,
+    population:8511621,
+    GDP:1540.6,
+    energyconsumed:614409
+},
+{
+    year :2000-01,
+    state :qld,
+    population: 3571469,
+    GDP:1020,
+    energyconsumed:192765
+},
+{
+    year :2001-02,
+    state:qld,
+    population:3653123,
+    GDP:1057.8,
+    energyconsumed:205841},
+{
+    year :2002-03,
+    state:qld,
+    population:3743121,
+    GDP:1064,
+    energyconsumed:212503
+},
+{
+    year :2003-04,
+    state:qld,
+    population:3829970,
+    GDP:1081.1,
+    energyconsumed:225939},
+{
+    year:2004-05,
+    state:qld,
+    population:3918494,
+    GDP:1225.1,
+    energyconsumed:238219
+},
+{
+    year :2005-06,
+    state:qld,
+    population:4007992,
+    GDP:1279.5,
+    energyconsumed:250498
+},
+{
+    year:2006-07,
+    state:qld,
+    population:4111018,
+    GDP:1329,
+    energyconsumed:267333
+},
+{
+    year:2007-08,
+    state:qld,
+    population:4219505,
+    GDP:1289.5,
+    energyconsumed:279738
+},
+{
+    year:2008-09,
+    state: qld,
+    population:4328771,
+    GDP:1346.7,
+    energyconsumed:283635
+},
+{
+    year :2009-10,
+    state :qld,
+    population:4404744,
+    GDP:1295.8,
+    energyconsumed: 287659
+},
+{
+    year :2010-11,
+    state :qld,
+    population:4476778,
+    GDP:1251.3,
+    energyconsumed:289605
+},
+{
+    year :2011-12,
+    state:qld,
+    population :4568687,
+    GDP:1316.4,
+    energyconsumed:305645
+},
+{
+    year :2012-13,
+    state :qld,
+    population :4652824,
+    GDP:1336.5,
+    energyconsumed:314029
+},
+{
+    year :2013-14,
+    state:qld,
+    population:4719653,
+    GDP:1337.9,
+    energyconsumed:320742
+},
+{
+    year :2014-15,
+    state:qld,
+    population:4777692,
+    GDP:1432,
+    energyconsumed:323849
+},
+{
+    year :2015-16,
+    state:qld,
+    population:4845152,
+    GDP:1460.2,
+    energyconsumed:331726
+},
+{
+    year :2016-17,
+    state:qld,
+    population :4927629,
+    GDP: 1529.6,
+    energyconsumed: 339652
+},
+{
+    year :2017-18,
+    state:qld,
+    population:5009424,
+    GDP:1552.5,
+    energyconsumed:352248
+},
+{
+    year :2018-19,
+    state:qld,
+    population:5092526,
+    GDP:1525.3,
+    energyconsumed:357044
+},
+{
+    year :2000-01,
+    state: sa,
+    population :1503461,
+    GDP:329.3,
+    energyconsumed:77122
+},
+{
+    year :2001-02,
+    state :sa,
+    population:1511567,
+    GDP:328.1,
+    energyconsumed:79882
+},
+{
+    year:2002-03,
+    state:sa,
+    population:1520399,
+    GDP:357.3,
+    energyconsumed:81310
+},
+{
+    year :2003-04,
+    state :sa,
+    population:1528189,
+    GDP:371.5,
+    energyconsumed:83202
+},
+{
+    year :2004-05,
+    state:sa,
+    population :1538804,
+    GDP:355.6,
+    energyconsumed:84430
+},
+{
+    year :2005-06,
+    state:sa,
+    population:1552529,
+    GDP:350.1,
+    energyconsumed:86297
+},
+{
+    year :2006-07,
+    state:sa,
+    population:1570619,
+    GDP:375.5,
+    energyconsumed:88355
+},
+{
+    year :2007-08,
+    state:sa,
+    population :1588665,
+    GDP:372.5,
+    energyconsumed:92502
+},
+
+{
+    year :  2008-09,
+    state :sa,
+    population :1608902,
+    GDP:357.1,
+    energyconsumed:95215
+},
+{
+    year:2009-10,
+    state :sa,
+    population:1627322,
+    GDP:343.5,
+    energyconsumed:97099
+},
+{
+    year :2010-11,
+    state :sa,
+    population:1639614,
+    GDP:342.6,
+    energyconsumed:98612
+},
+{
+    year:2011-12,
+    state: sa,
+    population:1656725,
+    GDP:307.5,
+    energyconsumed:99569
+},
+{
+    year :2012-13,
+    state:sa,
+    population:1671488,
+    GDP:356.2,
+    energyconsumed:100633
+},
+{
+    year:2013-14,
+    state:sa,
+    population:1686945,
+    GDP:343.5,
+    energyconsumed:101438
+},
+{
+    year: 2014-15,
+    state:sa,
+    population:1700668,
+    GDP:363.4,
+    energyconsumed:102333
+},
+{
+    year:2015-16,
+    state:sa,
+    population:1712843,
+    GDP:323.5,
+    energyconsumed:102602
+},
+{
+    year :2016-17,
+    state :sa,
+    population:1723923,
+    GDP:295.1,
+    energyconsumed:104125
+},
+{
+    year: 2017-18,
+    state :sa,
+    population:1736527,
+    GDP:335.5,
+    energyconsumed:106477
+},
+{
+    year :2018-19,
+    state :sa,
+    population:1752326,
+    GDP:322.6,
+    energyconsumed:107990
+},
+{
+    year:2000-01,
+    state:tas,
+    population:473668,
+    GDP:93.6,
+    energyconsumed:21192
+},
+{
+    year:2001-02,
+    state:tas,
+    population:474152,
+    GDP:97.2,
+    energyconsumed:22129
+},
+{
+    year :2002-03,
+    state: tas,
+    population:478534,
+    GDP:99.5,
+    energyconsumed:22766
+},
+{
+    year:2003-04,
+    state :tas,
+    population:483178,
+    GDP:103.4,
+    energyconsumed:23912
+},
+{
+    year: 2004-05,
+    state:tas,
+    population:486202,
+    GDP:107.4,
+    energyconsumed:24390
+},
+{
+    year :2005-06,
+    state:tas,
+    population:489302,
+    GDP:111.6,
+    energyconsumed:25176
+},
+{
+    year :2006-07,
+    state:tas,
+    population:493262,
+    GDP:113.3,
+    energyconsumed:25947
+},
+{
+    year:2007-08,
+    state:tas,
+    population:498568,
+    GDP:115.5,
+    energyconsumed:26839
+},
+{
+    year:2008-09,
+    state:tas,
+    population :504353,
+    GDP:114.9,
+    energyconsumed:27375
+},
+{
+    year :2009-10,
+    state:tas,
+    population :508847,
+    GDP:114.9,
+    energyconsumed:27605
+},
+{
+    year :2010-11,
+    state :tas,
+    population :511483,
+    GDP:114.9,
+    energyconsumed :28130
+},
+{
+    year:2011-12,
+    state :tas,
+    population :511724,
+    GDP:108.1,
+    energyconsumed:28333
+},
+{
+    year :2012-13,
+    state:tas,
+    population :512231,
+    GDP:113.5,
+    energyconsumed :28259
+},
+{
+    year :2013-14,
+    state:tas,
+    population :513621,
+    GDP:108.7,
+    energyconsumed:28552
+},
+{
+    year :2014-15,
+    state:tas,
+    population :515117,
+    GDP:105.4,
+    energyconsumed:28865
+},
+{
+    year :2015-16,
+    state:tas,
+    population:517514,
+    GDP:107.5,
+    energyconsumed:29364
+},
+{
+    year :2016-17,
+    state:tas,
+    population :522410,
+    GDP:107.9,
+    energyconsumed:29685
+},
+{
+    year:2017-18,
+    state:tas,
+    population :528298,
+    GDP:110.9,
+    energyconsumed:30710
+},
+
+{
+    year :2018-19,
+    state:tas,
+    population :534599,
+    GDP:108.8,
+    energyconsumed:31819
+},
+{
+    year:2000-01,
+    state:vic,
+    population:4763615,
+    GDP:1337.2,
+    energyconsumed:268713
+},
+{
+    year: 2001-02,
+    state:vic,
+    population :4817774,
+    GDP:1371.9,
+    energyconsumed:279448
+},
+{
+    year:2002-03,
+    state:vic,
+    population:4873809,
+    GDP:1330.6,
+    energyconsumed:289224
+},
+{
+    year :2003-04,
+    state:vic,
+    population:4927149,
+    GDP:1364.9,
+    energyconsumed:298886
+},
+{
+    year:2004-05,
+    state:vic,
+    population :4989246,
+    GDP:1356.5,
+    energyconsumed:311197
+},
+{
+    year :2005-06,
+    state :vic,
+    population :5061266,
+    GDP:1429.5,
+    energyconsumed:317619
+},
+{
+    year :2006-07,
+    state :vic,
+    population :5153522,
+    GDP:1433.9,
+    energyconsumed:326877
+},
+{
+    year :2007-08,
+    state :vic,
+    population :5256375,
+    GDP:1426.1,
+    energyconsumed:338154
+},
+{
+    year :2008-09,
+    state :vic,
+    population :5371934,
+    GDP:1418.9,
+    energyconsumed:344311
+},
+{
+    year :2009-10,
+    state:vic,
+    population :5461101,
+    GDP:1424.3,
+    energyconsumed:348730
+},
+{
+    year :2010-11,
+    state: vic,
+    population :5537817,
+    GDP:1446,
+    energyconsumed:358943
+},
+{
+    year :2011-12,
+    state:vic,
+    population:5651091,
+    GDP:1454.3,
+    energyconsumed:366246
+},
+{
+    year :2012-13,
+    state:vic,
+    population :5772669,
+    GDP:1422.4,
+    energyconsumed:370232
+},
+{
+    year :2013-14,
+    state:vic,
+    population:5894917,
+    GDP:1420.7,
+    energyconsumed:378812
+},
+{
+    year :2014-15,
+    state:vic,
+    population:6022322,
+    GDP:1403.5,
+    energyconsumed:389391
+},
+{
+    year:2015-16,
+    state:vic,
+    population:6173172,
+    GDP:1430.4,
+    energyconsumed:402564
+},
+{
+    year:2016-17,
+    state:vic,
+    population:6321606,
+    GDP:1400.6,
+    energyconsumed:418885
+},
+{
+    year :2017-18,
+    state:vic,
+    population:6462019,
+    GDP:1317.4,
+    energyconsumed:432993
+},
+{
+    year:2018-19,
+    state:vic,
+    population:6593884,
+    GDP:1297.6,
+    energyconsumed:446079
+},
+{
+    year :2000-01,
+    state: wa,
+    population :1906274,
+    GDP:708.1,
+    energyconsumed:122969
+},
+{
+    year :2001-02,
+    state :wa,
+    population :1928512,
+    GDP:722.9,
+    energyconsumed:131200
+},
+{
+    year :2002-03,
+    state:wa,
+    population:1952741,
+    GDP:720,
+    energyconsumed:138059
+},
+{
+    year :2003-04,
+    state:wa,
+    population :1979542,
+    GDP:730.3,
+    energyconsumed:146469
+},
+{
+    year :2004-05,
+    state:wa,
+    population:2011207,
+    GDP:748.2,
+    energyconsumed:151620
+},
+{
+    year :2005-06,
+    state:wa,
+    population:2050581,
+    GDP:757.1,
+    energyconsumed:158729
+},
+{
+    year :2006-07,
+    state:wa,
+    population:2106139,
+    GDP:820.7,
+    energyconsumed:168938
+},
+{
+    year :2007-08,
+    state:wa,
+    population:2171700,
+    GDP:828,
+    energyconsumed:177371
+},
+{
+    year :2008-09,
+    state:wa,
+    population:2240250,
+    GDP:918.8,
+    energyconsumed:183919
+},
+{
+    year :2009-10,
+    state:wa,
+    population :2290845,
+    GDP:907.5,
+    energyconsumed:192854
+},
+{
+    year :2010-11,
+    state:wa,
+    population:2353409,
+    GDP:981.8,
+    energyconsumed:201434
+},
+{
+    year :2011-12,
+    state:wa,
+    population:2425507,
+    GDP:983.6,
+    energyconsumed:220648
+},
+{
+    year :2012-13,
+    state:wa,
+    population :2486944,
+    GDP:1020.3,
+    energyconsumed:233689
+},
+{
+    year :2013-14,
+    state:wa,
+    population:2517608,
+    GDP:1067.3,
+    energyconsumed:247281
+},
+{
+    year :2014-15,
+    state:wa,
+    population:2540672,
+    GDP:1041.8,
+    energyconsumed:253346
+},
+{
+    year :2015-16,
+    state:wa,
+    population :2555978,
+    GDP:1120.7,
+    energyconsumed:256383
+},
+{
+    year :2016-17,
+    state:wa,
+    population:2574193,
+    GDP:1172.9,
+    energyconsumed:251941
+},
+{
+    year :2017-18,
+    state:wa,
+    population :2594181,
+    GDP: 1206.6,
+    energyconsumed:258120
+},
+{
+    year :2018-19,
+    state:wa,
+    population :2621836,
+    GDP:1261,
+    energyconsumed:260640
+},
+{
+    year :2000-01,
+    state:nt, 
+    population:201743,
+    GDP:73.2,
+    energyconsumed:14361
+},
+{
+    year :2001-02,
+    state: nt,
+    population:202251,
+    GDP:73.4,
+    energyconsumed:15378
+},
+{
+    year :2002-03,
+    state:nt,
+    population:201725,
+    GDP:56.8,
+    energyconsumed:15471
+},
+{
+    year :2003-04,
+    state :nt,
+    population :202663,
+    GDP:58.7,
+    energyconsumed:15570
+},
+{
+    year :2004-05,
+    state:nt,
+    population :205905,
+    GDP:61.5,
+    energyconsumed:15787
+},
+{
+    year :2005-06,
+    state:nt,
+    population :209057,
+    GDP:73,
+    energyconsumed:17010
+},
+{
+    year :2006-07,
+    state:nt,
+    population :213748,
+    GDP:82.8,
+    energyconsumed:18791
+},
+{
+    year :2007-08,
+    state:nt,
+    population:219874,
+    GDP:96,
+    energyconsumed:19162
+},
+{
+    year :2008-09,
+    state:nt,
+    population :226027,
+    GDP:95.1,
+    energyconsumed:20437
+},
+{
+    year:2009-10,
+    state:nt,
+    population:229778,
+    GDP:94.2,
+    energyconsumed:20147
+},
+{
+    year :2010-11,
+    state :nt,
+    population :231292,
+    GDP:99.8,
+    energyconsumed :20635
+},
+{
+    year :2011-12,
+    state: nt,
+    population :235915,
+    GDP:98.2,
+    energyconsumed: 21607
+},
+{
+    year :2012-13,
+    state:nt,
+    population :241722,
+    GDP:100.1,
+    energyconsumed:24189
+},
+{
+    year:2013-14,
+    state:nt,
+    population:242894,
+    GDP:99.1,
+    energyconsumed:24502
+},
+{
+    year:2014-15,
+    state :nt,
+    population:244692,
+    GDP:83.7,
+    energyconsumed:25105
+},
+{
+    year:2015-16,
+    state :nt,
+    population:245678,
+    GDP:83.9,
+    energyconsumed:25601
+},
+{
+    year:2016-17,
+    state:nt,
+    population:247517,
+    GDP:83.2,
+    energyconsumed:25977
+},
+{
+    year :2017-18,
+    state:nt,
+    population:247058,
+    GDP:91.1,
+    energyconsumed:26501
+},
+{
+    year :2018-19,
+    state: nt,
+    population:246103,
+    GDP:140.1,
+    energyconsumed:26109
+}];
