26cc0f1ce564b46a583a0462a6599fb789e5353a
presitkaur <presitkaur@users.noreply.github.com>
2021-04-27 16:31:03 +0800

    Updated: Added selector and about routes

--- web-stuff/main.py
+++ web-stuff/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route ("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/selector")
+def selector(): 
+    return render_template("selector.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
