e6bff2e78ddd84ef8c550f1f857155978971d541
unknown <maravenables@gmail.com>
2021-04-28 22:37:35 +0800

    testing flask

--- web-stuff/static/js/mara.js
+++ web-stuff/static/js/mara.js
@@ -1,17 +1,17 @@
 // https://github.com/chartjs/Chart.js
 // https://www.chartjs.org/docs/latest/axes/styling.html
 function buildPlot() {
-  /* data route */
-  const url = "/api/popGb";
-  d3.json(url).then(function(response) {
-
-    console.log(response);
-
-    const data = response;
-
-
-       
-  });
-}
-
-buildPlot();
+    /* data route */
+    const url = "/api/popGb";
+    d3.json(url).then(function(response) {
+  
+      console.log(response);
+  
+      const data = response;
+  
+  
+         
+    });
+  }
+  
+  buildPlot();
