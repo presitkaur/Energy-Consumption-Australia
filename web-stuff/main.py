74bef8722539fe86cdc664ac53b48656928a7bb4
presitkaur <presitkaur@users.noreply.github.com>
2021-04-28 20:13:01 +0800

    Updated version"

--- web-stuff/main.py
+++ web-stuff/main.py
@@ -89,13 +89,13 @@ def home():
 def about():
     return render_template("about.html")
 
-# @app.route("/selector")
-# def selector(): 
-#     return render_template("selector.html")
-
-@app.route("/mara")
+@app.route("/selector")
 def selector(): 
-    return render_template("mara.html")
+    return render_template("selector.html")
+
+@app.route("/plot_template")
+def plot_template(): 
+    return render_template("plot_template")
 
 @app.route("/api/pop")
 def pop():
