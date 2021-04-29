7f4de851b159631142d107a3046f52985cbee157
Aroosa-Github <aroosa@outlook.com>
2021-04-29 13:10:42 +0800

    added files

---
+++ aroosa/bubble.js
@@ -0,0 +1,39 @@
+var trace1 = {
+    x: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+    y: [3996.50,3901.80,3859.60,3705.00,3628.60,3524.60,3452.50,3441.10,3402.60,3329.50,3287.00],
+    text: ['Energy Productivity', '<br>Energy Productivity', '<br>Energy Productivity', '<br>Energy Productivity','<br>Energy Productivity', '<br>Energy Productivity', '<br>Energy Productivity'],
+    // text:['A<br>Energy Productivity', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+    mode: 'markers',
+    // name :'Energy Productivity'
+    marker: {
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)','rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 
+      'rgb(255, 65, 54)', 'rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)'],
+      size: [40, 50, 60, 70,80,90, 100,40,60,60, 100]
+      ,
+    }
+  };
+  var trace2 = {
+    x: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+    // y: [5843.80,5823.30,5902.20,5887.80,5915.30,5891.40,5897.30,6040.80,6114.60,6159.10,6196.00],
+    y: [250.20,256.30,259.10,269.90,275.60,283.70,289.60,290.60,293.90,300.30,304.20],
+    text: ['Energy Intensity', 'Energy Intensity', 'Energy Intensity','Energy Intensity', 'Energy Intensity','Energy Intensity', 'Energy Intensity','Energy Intensity',],
+    mode: 'markers',
+    marker: {
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)','rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 
+      'rgb(255, 65, 54)', 'rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)'],
+      size: [40, 50,50, 60, 70,80,80,60,60,80,100]
+    }
+  };
+  
+  var data = [trace1, trace2];
+  
+  var layout = {
+    title: 'Australian energy Productivity and energy intensity',
+    xtitle: "Years",
+    ytitle: "Energy",
+    showlegend: false,
+    height: 600,
+    width: 600
+  };
+  
+  Plotly.newPlot('plot', data, layout);
---
+++ aroosa/bubble2.js
@@ -0,0 +1,71 @@
+var dotDiv = document.getElementById("dot-chart");
+ 
+var traceA = {
+  type: "scatter",
+  mode: "markers",
+  x: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+  // y: [5843.80,5823.30,5902.20,5887.80,5915.30,5891.40,5897.30,6040.80,6114.60,6159.10,6196.00],
+  y: [2502,2563,2591,2699,2756,2837,2896,2906,2939,3003,3042],
+  name: 'Energy Productivity',
+  marker: {
+    color: 'rgba(156, 165, 196, 0.5)',
+    line: {
+      color: 'rgba(156, 165, 196, 1)',
+      width: 1,
+    },
+    symbol: 'circle',
+    size: 20
+  },
+  hoverlabel: {
+    bgcolor: 'black',
+  }
+};
+ 
+var traceB = {
+  type: "scatter",
+  mode: "markers",
+  x: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+    y: [3996.50,3901.80,3859.60,3705.00,3628.60,3524.60,3452.50,3441.10,3402.60,3329.50,3287.00],
+  name: 'Energy Intensity',
+  marker: {
+    color: 'rgba(165, 196, 50, 0.5)',
+    line: {
+      color: 'rgba(165, 196, 50, 1)',
+      width: 1,
+    },
+    symbol: 'circle',
+    size: 20
+  },
+  hoverlabel: {
+    bgcolor: 'black',
+  }
+};
+ 
+var data = [traceA, traceB];
+ 
+var layout = {
+  title: 'Australian energy Productivity and energy intensity',
+  xaxis: {
+    showgrid: false,
+    showline: true,
+    linecolor: 'rgb(200, 0, 0)',
+    ticks: 'outside',
+    tickcolor: 'rgb(200, 0, 0)',
+    tickwidth: 4
+  },
+  legend: {
+    bgcolor: 'white',
+    borderwidth: 1,
+    bordercolor: 'black',
+    orientation: 'h',
+    xanchor: 'center',
+    x: 0.5,
+    font: {
+      size: 12,
+    }
+  },
+  paper_bgcolor: 'rgb(255, 230, 255)',
+  plot_bgcolor: 'rgb(255, 230, 255)'
+};
+ 
+Plotly.plot(plot, data, layout);
